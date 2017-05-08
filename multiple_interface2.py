f81481d636dab19fa5fa08e56eb2f70bff6a8b5a
C.Y Wu <cywu@smail.nchu.edu.tw>
2017-05-08 15:19:31 +0800

    Add a topo for lab3

---
+++ multiple_interface2.py
@@ -0,0 +1,42 @@
+"""Custom topology example
+
+Two directly connected switches plus a host for each switch:
+
+   host --- switch --- switch --- host
+
+Adding the 'topos' dict with a key/value pair to generate our newly defined
+topology enables one to pass in '--topo=mytopo' from the command line.
+"""
+
+from mininet.topo import Topo
+from mininet.node import Host,Node
+from mininet.util import ensureRoot, waitListening
+from mininet.log import info, warn, output
+class MyTopo( Topo ):
+    "Simple topology example."
+
+    def __init__( self ):
+        "Create custom topo."
+
+        # Initialize topology
+        Topo.__init__( self )
+
+        # Add hosts and switches
+        Host1 = self.addHost( 'h1' )
+
+        Switch1 = self.addSwitch( 's1' )
+        Switch2 = self.addSwitch( 's2' )
+	Switch3 = self.addSwitch( 's3' )
+
+        # Add links
+        self.addLink( Switch1, Switch2 )
+        self.addLink( Switch1, Switch3 )
+        self.addLink( Switch2, Host1 ,addr2='00:00:00:00:00:01')
+	self.addLink( Switch2, Host1 ,addr2='00:00:00:00:00:02')
+	self.addLink( Switch3, Host1 ,addr2='00:00:00:00:00:03')
+	self.addLink( Switch3, Host1 ,addr2='00:00:00:00:00:04')
+	
+	
+
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
